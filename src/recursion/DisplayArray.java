8839d8dee2fb8e1a253fcb6dde7132ca69b5fa72
priyanka_varma <priyankavarma717@gmail.com>
2020-11-24 17:35:39 +0530

    Add reverse array

--- src/recursion/DisplayArray.java
+++ src/recursion/DisplayArray.java
@@ -10,7 +10,10 @@ public class DisplayArray {
         for(int i=0;i<arr.length;i++){
             arr[i] = scanner.nextInt();
         }
+        System.out.println("Display Array with Recursion");
         displayArray(arr, 0);
+        System.out.println("Display Array in Reverse with Recursion");
+        displayReverseArray(arr, 0);
     }
 
     private static void displayArray(int[] arr, int i) {
@@ -20,24 +23,37 @@ public class DisplayArray {
         System.out.println(arr[i]);
         displayArray( arr, i+1 );
     }
+
+    private static void displayReverseArray(int [] arr, int i){
+        if(i == arr.length){
+            return;
+        }
+        displayReverseArray( arr, i+1 );
+        System.out.println(arr[i]);
+    }
 }
 
 
 /** Display Array using Recursion
 Input
-n=5
-3
-1
-0
-7
-5
-Output
-3
-1
-0
-7
-5
-
-Process finished with exit code 0
+ 5
+ 10
+ 20
+ 30
+ 40
+ 50
+ Output
+ Display Array with Recursion
+ 10
+ 20
+ 30
+ 40
+ 50
+ Display Array in Reverse with Recursion
+ 50
+ 40
+ 30
+ 20
+ 10
 
  */
