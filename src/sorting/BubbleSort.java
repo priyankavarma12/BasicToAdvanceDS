5fce5e3c278a73d591efeb62b83d0ee3b48a9a8a
priyanka_varma <priyankavarma717@gmail.com>
2020-11-27 19:02:39 +0530

    Bubble Sort

---
+++ src/sorting/BubbleSort.java
@@ -0,0 +1,83 @@
+package sorting;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+        for(int itr=1; itr<=arr.length-1; itr++){
+            for(int j=0;j<arr.length-itr; j++){
+                if(isSmaller(arr , j+1, j)){
+                    swap(arr, j+1, j);
+                }
+            }
+        }
+
+    }
+
+    // used for swapping ith and jth elements of array
+    public static void swap(int[] arr, int i, int j) {
+        System.out.println("Swapping " + arr[i] + " and " + arr[j]);
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    // return true if ith element is smaller than jth element
+    public static boolean isSmaller(int[] arr, int i, int j) {
+        System.out.println("Comparing " + arr[i] + " and " + arr[j]);
+        if (arr[i] < arr[j]) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        bubbleSort(arr);
+        print(arr);
+    }
+}
+
+
+/** Bubble Sort
+5
+7
+-2
+4
+1
+3
+Comparing -2 and 7
+Swapping -2 and 7
+Comparing 4 and 7
+Swapping 4 and 7
+Comparing 1 and 7
+Swapping 1 and 7
+Comparing 3 and 7
+Swapping 3 and 7
+Comparing 4 and -2
+Comparing 1 and 4
+Swapping 1 and 4
+Comparing 3 and 4
+Swapping 3 and 4
+Comparing 1 and -2
+Comparing 3 and 1
+Comparing 1 and -2
+-2
+1
+3
+4
+7
+ */
